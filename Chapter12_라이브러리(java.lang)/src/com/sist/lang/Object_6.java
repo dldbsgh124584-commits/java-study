1fd75b4907b8d2f9bc3c4be6e05d37883b5662db
dldbsgh124584 <sist-17@sist17>
2026-04-01 17:41:29 +0900

    03 java

---
+++ Chapter12_라이브러리(java.lang)/src/com/sist/lang/Object_6.java
@@ -0,0 +1,27 @@
+package com.sist.lang;
+/*
+ *     clone / toString / hashCode / equals 
+ *     -------------------------------------
+ *     => record
+ *     class record Sawon
+ *     {
+ *       => 변수 설정 => getter 만 사용 : Spring AI => 전송 
+ *     }
+ * 
+ */
+// 508p
+// 보안 => 데이터를 모아서 브라우저 전송할 목적 .. => VO(getter/setter)
+record Person(String name,int age)
+{
+	
+}
+public class Object_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Person p=new Person("홍길동", 20);
+        System.out.println("이름:"+p.name()); // getter
+        System.out.println("나이:"+p.age());
+	}
+
+}
